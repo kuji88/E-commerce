ed9fae5d2e4d3356cb728553d98355ec6917f61f
Kuji <kuji99@aol.com>
2023-08-17 17:56:18 +0300

    Global Error handler

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ dotenv.config({ path:"config.env"})
 const morgan = require("morgan")
 const DB = require('./config/Database')
 const { CatagoryRouter } = require('./routers/CatagoriesRoute')
+const ApiError = require('./utils/ApiError')
 
 
 //* Express app
@@ -20,6 +21,25 @@ if(process.env.NODE_ENV === "development"){
 
 //!Routes
 app.use('/api/v1',CatagoryRouter)
+
+//* Route Error
+app.all('*',(req,res,next)=>{
+    
+    next(ApiError(`Can't find this route${req.originalUrl}`,404))
+})  
+
+//* Globale handler
+app.use((err,req,res,next)=>{
+    err.statusCode = err.statusCode || 500
+    err.status = err.status || 'Error'
+    res.status(404).json({
+        status: err.status,
+        statusCode: err.statusCode,
+        message: err.message,
+        stack: err.stack
+    })
+
+})
 
 
 //* Server creator
